2c53ed0695e3268d383a91d928f6cc148cbf5fc9
injoon5 <52849281+injoon5@users.noreply.github.com>
2020-06-17 18:51:31 +0900

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,28 +1,36 @@
-import random as r
+import random asrandom as r
 
-fruitlist = ['apple', 'banana', 'orange']
+      fruitlist = ['apple', 'banana', 'orange', 'mango', 'strawberry', 'hanlabong', 'grape', 'blueberry', ]
 
-myfruit = r.choice(fruitlist)
+      myfruit = r.choice(fruitlist)
 
-myfruitlist = list(myfruit)
+      myfruitlist = list(myfruit)
 
-r.shuffle(myfruitlist)
+      r.shuffle(myfruitlist)
 
-whatfruit = ' '.join(myfruitlist)
+      whatfruit = ' '.join(myfruitlist)
 
-print("힌트:", whatfruit)
+      print("힌트:", whatfruit)
 
 
-while True:
-    a = input("answer:")
+      while True:
+          a = input("answer:")
     
-    if a != myfruit:
-        print('try again')
-        continue
+          if a != myfruit:
+              print('try again')
+              continue
         
-    else:
-        break
+          else:
+              break
         
-print("you won!")
+      print("you won!",'to try again, press 1.')
+      startagain = input('press 1 to restart, press anything else to end.')
+      if startagain == "1":
+                       
+                       continue
+      else:
+                       break
+      
+      
 
         
