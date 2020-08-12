7392f07602cfabbc513eca826759dc080c6091b9
오인준 <injoon5@naver.com>
2020-08-12 19:54:27 +0900

    Create linusannasplit.py

---
+++ linusannasplit.py
@@ -0,0 +1,20 @@
+linus = "Talk is cheap. Show me the code. - Linus Torvals"
+anna = ['Do', 'you', 'want', 'to', 'build', 'a', 'snowman?']
+split1 = linus.split(" ")
+split2 = linus.split(" ")
+split3= linus.split(" ", 3)
+print(split1)
+print(split2)
+print(split3, end='\n\n')
+
+join1 = ' '.join(anna)
+join2 = '+'.join(split1)
+join3 = '_'.join(split1)
+print(join1)
+print(join2)
+print(join3)
+print('-'.join(['010', '9987', '4567']))
+print(''.join(['010', '9987', '4567']))
+#---------------------------------------
+#Coded by injoon.
+#Github: https://github.com/injoon5
