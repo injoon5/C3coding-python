fdbc4ca5c4178d633d8c297d42d558ceb3b13811
injoon <injoon5@naver.com>
2020-09-23 19:53:25 +0900

    Create 1.py

---
+++ 20200923/1.py
@@ -0,0 +1,19 @@
+class Car:
+      speed = 'low'
+      gear = '2'
+      color = 'blue'
+
+ccubeCar1 = Car()
+ccubeCar2 = Car()
+
+ccubeCar2.speed = 'high'
+ccubeCar2.gear = '3'
+ccubeCar2.color = 'black'
+
+print(ccubeCar1.speed)
+print(ccubeCar1.gear)
+print(ccubeCar1.color)
+
+print(ccubeCar2.speed)
+print(ccubeCar2.gear)
+print(ccubeCar2.color)
