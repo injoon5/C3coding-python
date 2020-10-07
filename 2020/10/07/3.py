b371547fdf85f8662f2b274f194391fee8a3018e
injoon <injoon5@naver.com>
2020-10-07 19:49:42 +0900

    Create 3.py

---
+++ 2020/10/07/3.py
@@ -0,0 +1,6 @@
+class Man:
+      def __init__(self,name,age):
+            self.name = name
+            self.age = age
+            print("%s is %d years old.  "%(self.name, self.age))
+m = Man('James', 33)
